7c6461ba4c4681514640c2a3a8bd84a2f67bd2c8
mareiirie <arinomamanohito@gmail.com>
2019-10-04 17:21:43 +0900

    レビュー後修正

--- 2-1-4/2-1-4UIalertview/ViewController.swift
+++ 2-1-4/2-1-4UIalertview/ViewController.swift
@@ -15,19 +15,23 @@ class ViewController: UIViewController {
         // ① UIAlertControllerクラスのインスタンスを生成
         // タイトル, メッセージ, Alertのスタイルを指定する
         // 第3引数のpreferredStyleでアラートの表示スタイルを指定する
-        let alert: UIAlertController = UIAlertController(title: "アラート表示", message: "焼酎をボトルで３本入れちゃいますか？", preferredStyle:  UIAlertController.Style.alert)
+        let alert: UIAlertController = UIAlertController(title: "アラート表示",
+                                                    message:"焼酎をボトルで３本入れちゃいますか？", preferredStyle:  UIAlertController.Style.alert)
         
         // ② Actionの設定
         // Action初期化時にタイトル, スタイル, 押された時に実行されるハンドラを指定する
         // 第3引数のUIAlertActionStyleでボタンのスタイルを指定する
         // OKボタン
-        let defaultAction: UIAlertAction = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler:{
+        let defaultAction: UIAlertAction = UIAlertAction(title: "OK",
+                                                         style: UIAlertAction.Style.default,
+                                                         handler:{
             // ボタンが押された時の処理を書く（クロージャ実装）
             (action: UIAlertAction!) -> Void in
             print("OK")
         })
         // キャンセルボタン
-        let cancelAction: UIAlertAction = UIAlertAction(title: "キャンセル", style: UIAlertAction.Style.cancel, handler:{
+        let cancelAction: UIAlertAction = UIAlertAction(title: "キャンセル",
+                                                        style: UIAlertAction.Style.cancel, handler:{
             // ボタンが押された時の処理を書く（クロージャ実装）
             (action: UIAlertAction!) -> Void in
             print("Cancel")
