a226588e448970d37bb883739683f44a96fabc54
mareiirie <arinomamanohito@gmail.com>
2019-10-10 20:08:34 +0900

    仮

--- 2-1-14/2-1-13/ViewController.swift
+++ 2-1-14/2-1-13/ViewController.swift
@@ -14,7 +14,7 @@ UICollectionViewDelegate {
 let photos1 = ["ジョルノジョバーナ", "ミスタ", "リゾット", "ナランチャ", "ブチャラティ"]
 let photos2 = ["ディオ", "ポルナレフ", "花京院", "承太郎"]
 let photos3 = ["吉良吉影"]
-let sections = ["第５部登場人物","第３部登場人物","第４部登場人物"]
+let sectionTitle = ["第５部登場人物","第３部登場人物","第４部登場人物"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,7 +23,7 @@ let sections = ["第５部登場人物","第３部登場人物","第４部登場
     func collectionView(_ collectionView: UICollectionView,
                         cellForItemAt indexPath: IndexPath) -> UICollectionViewCell{
         
-        // "Cell" はストーリーボードで設定したセルのID
+        // "cell" はストーリーボードで設定したセルのID
         let testCell:UICollectionViewCell =
             collectionView.dequeueReusableCell(withReuseIdentifier: "cell",
                                                for: indexPath)
@@ -79,13 +79,36 @@ let sections = ["第５部登場人物","第３部登場人物","第４部登場
         }
     }
 
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
+  // private func collectionView(collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, atIndexPath indexPath: NSIndexPath) -> UICollectionReusableView
+   // {
+     //   let testSection = collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: "TestSection", for: indexPath as IndexPath) as! TestCollectionReusableView
+            
+            //ヘッダーの背景色は紫、ラベルにセクション名を設定する。
+       //     testSection.backgroundColor = UIColor(red: 0.7, green: 0.7,blue: 0.8, alpha: 1.0)
+       // testSection.testLabel.text = sectionTitle[indexPath.section]
+            
+         //   return testSection
+            
+        //}
+        
+    //override func didReceiveMemoryWarning() {
+      //  super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
+    //}
  
 
+
+    //reusableviewに値を渡すメソッド
+  func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+       var reusableView = UICollectionReusableView()
+       if kind == UICollectionView.elementKindSectionHeader {
+            let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: "HeaderView", for: indexPath)
+            if let label = headerView.viewWithTag(3) as? UILabel {
+                label.text = sectionTitle[indexPath.section]
+            }
+            reusableView = headerView
+        }
+        return reusableView
     }
 
-
+}
