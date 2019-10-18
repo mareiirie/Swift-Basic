4bcea17f223f45cf3ae0081df03c3fa6fbdebe8f
mareiirie <arinomamanohito@gmail.com>
2019-10-18 13:56:25 +0900

    rebyu-go

---
+++ 2-1-12new/2-1-13/CustomCell.swift
@@ -0,0 +1,18 @@
+//
+//  CustomCell.swift
+//  2-1-13
+//
+//  Created by 入江真礼 on 2019/10/18.
+//  Copyright © 2019 入江真礼. All rights reserved.
+//
+
+import UIKit
+
+class CustomCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
