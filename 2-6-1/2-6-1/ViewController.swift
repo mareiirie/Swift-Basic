afdee8fc3809b1c0c7934500665c247ddd465ef7
mareiirie <arinomamanohito@gmail.com>
2019-10-24 11:59:27 +0900

    2-6-1

---
+++ 2-6-1/2-6-1/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  2-6-1
+//
+//  Created by 入江真礼 on 2019/10/24.
+//  Copyright © 2019 入江真礼. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController  {
+
+    @IBOutlet weak var label: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
