b7b912e267507475271e9b5bfb7f66c0b77eaeeb
mareiirie <arinomamanohito@gmail.com>
2019-10-24 10:26:24 +0900

    2-5-9新規追加

---
+++ 2-5-9/2-5-9/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  2-5-9
+//
+//  Created by 入江真礼 on 2019/10/23.
+//  Copyright © 2019 入江真礼. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ 2-5-9/2-5-9Tests/__5_9Tests.swift
@@ -0,0 +1,34 @@
+//
+//  __5_9Tests.swift
+//  2-5-9Tests
+//
+//  Created by 入江真礼 on 2019/10/23.
+//  Copyright © 2019 入江真礼. All rights reserved.
+//
+
+import XCTest
+@testable import __5_9
+
+class __5_9Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
